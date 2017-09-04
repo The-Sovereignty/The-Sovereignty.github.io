83304d8793ab89063a2c763727012122c5a60302
DracaDragon <JasmineSMoniz@gmail.com>
2017-09-04 11:10:09 -0700

    fix of links and stylings for said links

--- js/changeBackground.js
+++ js/changeBackground.js
@@ -2,7 +2,7 @@
 
 function changeImg()
 {
-	var newImgNumber =Math.floor(Math.random()*6);
+	var newImgNumber =Math.floor(Math.random()*9);
 	document.getElementById('background').src = '../communitybackgrounds/background'+newImgNumber+'.png';
 }
 window.onload=changeImg;
